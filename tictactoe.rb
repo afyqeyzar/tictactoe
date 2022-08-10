80c36364d7226fa858f3ee3e1230f40ee56ec208
afyqeyzar <norafyqe@gmail.com>
2022-08-10 18:12:29 +0100

    added function to display grid

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,11 +1,20 @@
 class TicTacToe
     attr_reader :player_one, :player_two
     def initialize(player_one, player_two)
-       @player_one = player_one
-       @player_two = player_two
-   end  
+        @player_one = player_one
+        @player_two = player_two
+        @location_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+   end 
+   
+   def display_board
+        puts " #{@location_array[0]} | #{@location_array[1]} | #{@location_array[2]} "
+        puts " -- --- --"
+        puts " #{@location_array[3]} | #{@location_array[4]} | #{@location_array[5]} "
+        puts " -- --- --"
+        puts " #{@location_array[6]} | #{@location_array[7]} | #{@location_array[8]} "
+   end 
 end
 
 newGame = TicTacToe.new("p1","p2")
 
-p newGame.player_one
+newGame.display_board
