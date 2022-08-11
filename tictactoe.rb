9814fd0a5c3cf1bec4bb66fcd8d7d60f3eb06aaa
afyqeyzar <norafyqe@gmail.com>
2022-08-11 12:58:52 +0100

    added functions to get player moves

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,10 +1,10 @@
 class TicTacToe
     attr_reader :player_one, :player_two
     def initialize
-        print "Enter Player 1 Name: "
-        @player_one = gets
-        print "Enter Player 2 Name: "
-        @player_two = gets
+        #print "Enter Player 1 Name: "
+        @player_one = "raeyn"
+        #print "Enter Player 2 Name: "
+        @player_two = "amir"
         @location_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
     
@@ -20,9 +20,53 @@ class TicTacToe
     def get_players
         puts @player_one, @player_two
     end
+
+    def get_choice_for_p1
+        print "#{@player_one} enter desired location: "
+        @p1_location = gets.to_i
+    end
+
+    def get_choice_for_p2
+        print "#{@player_two} enter desired location: "
+        @p2_location = gets.to_i
+    end
+    
+    def change_square_for_p1
+        if @location_array[@p1_location - 1] == "O" or @location_array[@p1_location - 1] == "X"
+            puts "that square is occipied"
+            get_choice_for_p1()
+        else
+            @location_array[@p1_location - 1] = "X"
+            display_board()
+        end
+    end
+
+    def change_square_for_p2
+        if @location_array[@p2_location - 1] == "O" or @location_array[@p2_location - 1] == "X"
+            puts "that square is occipied"
+            get_choice_for_p2()
+        else
+            @location_array[@p2_location - 1] = "O"
+            display_board()
+        end
+    end
+
+    def player_one_turn
+        get_choice_for_p1()
+        change_square_for_p1()
+    end
+
+    def player_two_turn
+        get_choice_for_p2()
+        change_square_for_p2()
+    end
 end
 
 newGame = TicTacToe.new
 
 #newGame.get_players3
 newGame.display_board
+newGame.get_choice_for_p1
+newGame.change_square_for_p1
+newGame.get_choice_for_p2
+newGame.change_square_for_p2
